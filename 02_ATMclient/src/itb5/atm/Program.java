2e2d4dd3ab22a7077171b95e078f3cd981cd0d0a
Florian Hämmerle <haemmerle.florian@googlemail.com>
2015-11-02 11:30:32 +0100

    sample client application

--- 02_ATMclient/src/itb5/atm/Program.java
+++ 02_ATMclient/src/itb5/atm/Program.java
@@ -1,7 +1,39 @@
 package itb5.atm;
 
+import itb5.atm.IATM;
+import itb5.atm.IATMFactory;
+import java.rmi.Naming;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello ATM!");
+
+        IATMFactory atmFactory = (IATMFactory)Naming.lookup("rmi://localhost:1337/atmfactory");
+        IATM atm = atmFactory.createATM();
+
+        // get initial account balance
+        System.out.println("Initial Balances");
+        System.out.println("Balance(1): "+atm.getBalance(1));
+        System.out.println("Balance(2): "+atm.getBalance(2));
+        System.out.println("Balance(3): "+atm.getBalance(3));
+        System.out.println();
+        // make €1000 depoist in account 1 and get new balance
+        System.out.println("Depositting(1): 1000 ");
+        atm.deposit(1, 1000);
+        System.out.println("Balance(1): "+atm.getBalance(1));
+        // make €100 withdrawal from account 2 and get new balance
+        System.out.println("Withdrawing(2): 100 ");
+        atm.withdraw(2, 100);
+        System.out.println("Balance(2): "+atm.getBalance(2));
+        // make €500 deposit in account 3 and get new balance
+        System.out.println("Depositting(3): 500 ");
+        atm.deposit(3, 500);
+        System.out.println("Balance(3): "+atm.getBalance(3));
+        // get final account balance
+        System.out.println();
+        System.out.println("Final Balances");
+        System.out.println("Balance(1): "+atm.getBalance(1));
+        System.out.println("Balance(2): "+atm.getBalance(2));
+        System.out.println("Balance(3): "+atm.getBalance(3));
     }
 }
