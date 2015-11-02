f4acab1d4d5328b6e41e091e563f4ab151b5b960
Florian Hämmerle <haemmerle.florian@googlemail.com>
2015-11-02 11:24:34 +0100

    add implementation of the ATM factory

--- 02_ATMserver/src/itb5/atm/ATMFactoryImpl.java
+++ 02_ATMserver/src/itb5/atm/ATMFactoryImpl.java
@@ -4,6 +4,7 @@ import itb5.atm.IATM;
 import itb5.atm.IATMFactory;
 
 public class ATMFactoryImpl implements IATMFactory {
+    @Override
     public IATM createATM() {
         return new ATMImpl();
     }
