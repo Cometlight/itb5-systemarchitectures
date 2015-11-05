2352ad37977d1aa11cac7731a5c4340b15dc5465
Florian Hämmerle <haemmerle.florian@googlemail.com>
2015-11-05 09:56:03 +0100

    move MutableString to indsys.types

--- INDSYS/src/indsys/types/MutableString.java
+++ INDSYS/src/indsys/types/MutableString.java
@@ -1,4 +1,4 @@
-package indsys;
+package indsys.types;
 
 /**
  * Represents a mutable String. Its value can never be null.
@@ -7,14 +7,16 @@ public class MutableString {
 	private String _value;
 	
 	/**
-	 * Creates a new mutable string and sets its initial value to the empty string.
+	 * Creates a new mutable string and sets its initial value to the empty 
+	 * string.
 	 */
 	public MutableString() {
 		this("");
 	}
 	
 	/**
-	 * Creates a new mutable string and sets its initial value to the specified value.
+	 * Creates a new mutable string and sets its initial value to the specified
+	 * value.
 	 * 
 	 * @param initialValue the initial value of the string; may not be null.
 	 */
@@ -27,20 +29,24 @@ public class MutableString {
 	}
 	
 	/**
-	 * Sets the value of the string.
+	 * Sets the value of the string. If the specified value is null, the 
+	 * mutable string's value is set to the empty string.
 	 * 
 	 * @param newValue the new value of the string; may not be null.
 	 */
 	public void setValue(String newValue) {
 		if (newValue != null) {
 			this._value = newValue;
+		} else {
+			this._value = "";
 		}
 	}
 	
 	/**
 	 * Gets the value of the string.
 	 * 
-	 * @return the value of the string; it is ensured that the value is not null.
+	 * @return the value of the string; it is ensured that the value is not 
+	 * 		null.
 	 */
 	public String getValue() {
 		return this._value;
