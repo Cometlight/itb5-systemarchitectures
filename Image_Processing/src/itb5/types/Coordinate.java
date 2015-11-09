a2b932237090c45034a089b6eb0572a08f14fa73
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-11-09 16:21:23 +0100

    push

--- Image_Processing/src/itb5/types/Coordinate.java
+++ Image_Processing/src/itb5/types/Coordinate.java
@@ -1,11 +1,20 @@
 package itb5.types;
 
-public class Coordinate {
+public class Coordinate implements Comparable<Coordinate> {
 	public int _x;
 	public int _y;
 	
 	public Coordinate(int x, int y) {
 		_x = x;
 		_y = y;
+	}
+
+	@Override
+	public int compareTo(Coordinate o) {
+		if(o._x == this._x && o._y == this._y) {
+			return 0;
+		} else {
+			return 1; 	// FIXME
+		}
 	}
 }
