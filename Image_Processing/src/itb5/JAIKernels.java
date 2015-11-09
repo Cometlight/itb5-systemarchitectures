89a26cf62989387e09d0b75efb14c530450b7211
Florian Hämmerle <haemmerle.florian@googlemail.com>
2015-11-09 15:46:15 +0100

    cirlce the kernel

--- Image_Processing/src/itb5/JAIKernels.java
+++ Image_Processing/src/itb5/JAIKernels.java
@@ -4,12 +4,12 @@ import javax.media.jai.KernelJAI;
 
 public class JAIKernels {
 	public static final KernelJAI circle7 = new KernelJAI(7, 7, new float[] {
-			0, 0, 0, 1, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0,
 			0, 0, 1, 1, 1, 0, 0,
 			0, 1, 1, 1, 1, 1, 0,
-			1, 1, 1, 1, 1, 1, 1,
+			0, 1, 1, 1, 1, 1, 0,
 			0, 1, 1, 1, 1, 1, 0,
 			0, 0, 1, 1, 1, 0, 0,
-			0, 0, 0, 1, 0, 0, 0
+			0, 0, 0, 0, 0, 0, 0
 	});
 }
