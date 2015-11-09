67b1452d7bccdf771fa440115263d6607b64d6b5
Florian Hämmerle <haemmerle.florian@googlemail.com>
2015-11-09 15:45:01 +0100

    kernel constants

---
+++ Image_Processing/src/itb5/JAIKernels.java
@@ -0,0 +1,15 @@
+package itb5;
+
+import javax.media.jai.KernelJAI;
+
+public class JAIKernels {
+	public static final KernelJAI circle7 = new KernelJAI(7, 7, new float[] {
+			0, 0, 0, 1, 0, 0, 0,
+			0, 0, 1, 1, 1, 0, 0,
+			0, 1, 1, 1, 1, 1, 0,
+			1, 1, 1, 1, 1, 1, 1,
+			0, 1, 1, 1, 1, 1, 0,
+			0, 0, 1, 1, 1, 0, 0,
+			0, 0, 0, 1, 0, 0, 0
+	});
+}
