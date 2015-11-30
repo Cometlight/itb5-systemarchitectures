1aa845dc13c5b049a239af7edb562cc947ac2bb1
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-11-30 12:54:12 +0100

    For Flo

---
+++ 04_ImageBeans/src/beans/PropertySupportBean.java
@@ -0,0 +1,21 @@
+package beans;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+public abstract class PropertySupportBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	protected static final Logger log = Logger.getLogger(PropertySupportBean.class.getName());
+	
+	protected PropertyChangeSupport pcs;
+	
+	public void addPropertyChangeListener(PropertyChangeListener pcl) {
+		pcs.addPropertyChangeListener(pcl);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener pcl) {
+		pcs.removePropertyChangeListener(pcl);
+	}
+}
