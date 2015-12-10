b09508c8991aa3579ecb97669f3de0d27824eabd
Florian Hämmerle <haemmerle.florian@outlook.com>
2015-12-10 13:44:28 +0100

    bang bang a: wall behavior

--- 05_Skynet/controllers/BangBangA/BangBangA.java
+++ 05_Skynet/controllers/BangBangA/BangBangA.java
@@ -1,70 +1,90 @@
-// File:          BangBangA.java
-// Date:          
-// Description:   
-// Author:        
-// Modifications: 
-
-// You may need to add other webots classes such as
-//  import com.cyberbotics.webots.controller.DistanceSensor;
-//  import com.cyberbotics.webots.controller.LED;
-// or more simply:
-//  import com.cyberbotics.webots.controller.*;
 import com.cyberbotics.webots.controller.DifferentialWheels;
+import com.cyberbotics.webots.controller.DistanceSensor;
 
-// Here is the main class of your controller.
-// This class defines how to initialize and how to run your controller.
-// Note that this class derives Robot and so inherits all its functions
 public class BangBangA extends DifferentialWheels {
-  
-	private static int MAX_SPEED = 1000; // max. motor speed
-	
-  // You may need to define your own functions or variables, like
-  //  private LED led;
-  
-  // BangBangA constructor
-  public BangBangA() {
-      
-    // call the Robot constructor
-    super();
-    
-    // You should insert a getDevice-like function in order to get the
-    // instance of a device of the robot. Something like:
-    //  led = getLED("ledName");
-    
-  }
-    
-  // User defined function for initializing and running
-  // the BangBangA class
-  public void run() {
-    
-    // Main loop:
-    // Perform simulation steps of 64 milliseconds
-    // and leave the loop when the simulation is over
-    while (step(64) != -1) {
-    	System.out.println("This is awesome!");
-    	setSpeed(MAX_SPEED, MAX_SPEED);
-      // Read the sensors:
-      // Enter here functions to read sensor data, like:
-      //  double val = distanceSensor.getValue();
-      
-      // Process sensor data here
-      
-      // Enter here functions to send actuator commands, like:
-      //  led.set(1);
-    };
-    
-    // Enter here exit cleanup code
-  }
+	private static final int STEP_TIME = 8;
+	private static final int MIN_SPEED = 0;
+	private static final int MAX_SPEED = 1000;
+	private static final int PROXIMITY_SENSOR_L = 0; // sensor left
+	private static final int PROXIMITY_SENSOR_LF = 1; // sensor left front
+	private static final int PROXIMITY_SENSOR_RF = 2; // sensor right front
+	private static final int PROXIMITY_SENSOR_R = 3; // sensor right
+	private static final int PROXIMITY_SENSOR_MAX = 200; // maximum distance for sensors
 
-  // This is the main program of your controller.
-  // It creates an instance of your Robot subclass, launches its
-  // function(s) and destroys it at the end of the execution.
-  // Note that only one instance of Robot should be created in
-  // a controller program.
-  // The arguments of the main function can be specified by the
-  // "controllerArgs" field of the Robot node
-  public static void main(String[] args) {
-    BangBangA controller = new BangBangA();
-    controller.run();
-  }
+	private DistanceSensor[] proximitySensors;
+
+	public BangBangA() {
+		super();
+
+		proximitySensors = new DistanceSensor[] { 
+				getDistanceSensor("ps5"), 
+				getDistanceSensor("ps7"),
+				getDistanceSensor("ps0"), 
+				getDistanceSensor("ps2") 
+		};
+		
+		// enable all proximity sensors
+		for (int i = 0; i < proximitySensors.length; i++) {
+			proximitySensors[i].enable(10);
+		}
+
+		// You should insert a getDevice-like function in order to get the
+		// instance of a device of the robot. Something like:
+		// led = getLED("ledName");
+
+	}
+
+	// User defined function for initializing and running
+	// the BangBangController class
+	public void run() {
+
+		// Main loop:
+		// Perform simulation steps of 64 milliseconds
+		// and leave the loop when the simulation is over
+		while (step(STEP_TIME) != -1) {
+			// Read the sensors:
+			// Enter here functions to read sensor data, like:
+			// double val = distanceSensor.getValue();
+
+			// Process sensor data here
+			
+			//double l = proximitySensors[PROXIMITY_SENSOR_L].getValue();
+			//double r = proximitySensors[PROXIMITY_SENSOR_R].getValue();
+			
+			//setSpeed(MAX_SPEED * r / l, MAX_SPEED * l / r);
+
+			if (proximitySensors[PROXIMITY_SENSOR_L].getValue() > PROXIMITY_SENSOR_MAX
+					|| proximitySensors[PROXIMITY_SENSOR_LF].getValue() > PROXIMITY_SENSOR_MAX) {
+				driveRight();
+			} else if (proximitySensors[PROXIMITY_SENSOR_R].getValue() > PROXIMITY_SENSOR_MAX
+					|| proximitySensors[PROXIMITY_SENSOR_RF].getValue() > PROXIMITY_SENSOR_MAX) {
+				driveLeft();
+			} else {
+				driveForward();
+			}
+
+			// Enter here functions to send actuator commands, like:
+			// led.set(1);
+		}
+
+		// Enter here exit cleanup code
+	}
+
+
+	public static void main(String[] args) {
+		BangBangA controller = new BangBangA();
+		controller.run();
+	}
+
+	public void driveLeft() {
+		setSpeed(MIN_SPEED, MAX_SPEED);
+	}
+
+	public void driveRight() {
+		setSpeed(MAX_SPEED, MIN_SPEED);
+	}
+
+	public void driveForward() {
+		setSpeed(MAX_SPEED, MAX_SPEED);
+	}
 }
