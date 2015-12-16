6877f19cb79f3235aec207d521e5799a71d54147
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-12-16 15:11:31 +0100

    last push for robot ex. 1

--- 05_Skynet/controllers/ProportionalB/ProportionalB.java
+++ 05_Skynet/controllers/ProportionalB/ProportionalB.java
@@ -2,6 +2,9 @@ public class ProportionalB extends BaseRobot {
 	private static final int LIGHT_SENSOR_LOWERBOUND = 3100;
 	private static final int LIGHT_SENSOR_UPPERBOUND = 4000;
 
+	private double[][] k = { {0,    0,    0,   0.1, 0, 0, 0.25, 0.65},
+			 				 {0.65, 0.25, 0.1, 0,   0, 0, 0,    0} };
+	private double[] c = { 0, 0d };
 
 	public static void main(String[] args) {
 		ProportionalB controller = new ProportionalB();
@@ -10,12 +13,7 @@ public class ProportionalB extends BaseRobot {
 
 	@Override
 	protected void update() {
-		double[][] k = { {0,    0,    0,   0.1, 0, 0, 0.25, 0.65},
-						 {0.65, 0.25, 0.1, 0,   0, 0, 0,    0} };
-
 		double[] s = getLightSensorDataSmoothed(Sensor.FRONT_R, Sensor.FRONT_RIGHT, Sensor.RIGHT, Sensor.BACK_RIGHT, Sensor.BACK_LEFT, Sensor.LEFT, Sensor.FRONT_LEFT, Sensor.FRONT_L);
-		
-		double[] c = { 0, 0d };
 		
 		double[] a = Matrix.add( Matrix.multiply(k, s), c );
 		
@@ -25,7 +23,6 @@ public class ProportionalB extends BaseRobot {
 		
 		a[0] = Math.min(MAX_SPEED, a[0]);
 		a[1] = Math.min(MAX_SPEED, a[1]);
-				
 		
 		setSpeed(a[0], a[1]);
 	}
