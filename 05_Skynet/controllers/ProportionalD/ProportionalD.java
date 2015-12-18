ca37c98f89d0bf3eb4d0c1a63e89a50aa13631e0
Florian Hämmerle <haemmerle.florian@outlook.com>
2015-12-18 12:43:10 +0100

    add some comments

--- 05_Skynet/controllers/ProportionalD/ProportionalD.java
+++ 05_Skynet/controllers/ProportionalD/ProportionalD.java
@@ -1,13 +1,16 @@
 /**
- * In this controller, it is possible to achieve higher than recommended speeds on one wheel during turns.
+ * This robot follows the wall to the left. It can take turns to the right (in
+ * corners) and sharp turns to the left (i.e. on T-shaped wall structures).
  *
+ * In this controller, it is possible to achieve higher than recommended speeds
+ * on one wheel during turns!
  */
 public class ProportionalD extends BaseRobot {
 	private	double[][] k = { {1, 0, 0, 0, 0, 0, 0.5, 1, 0},
 							 {-1, 0, 0, 0, 0, 0, 0, 0, 2500}
 	   					   };
 	private double[] c = { 0, MAX_SPEED/6d };
-	
+
 	public static void main(String[] args) {
 		ProportionalD controller = new ProportionalD();
 		controller.run();
@@ -16,14 +19,14 @@ public class ProportionalD extends BaseRobot {
 	@Override
 	protected void update() {
 		double[] s = getDistanceSensorDataSmoothed(Sensor.FRONT_R, Sensor.FRONT_RIGHT, Sensor.RIGHT, Sensor.BACK_RIGHT, Sensor.BACK_LEFT, Sensor.LEFT, Sensor.FRONT_LEFT, Sensor.FRONT_L, Sensor.FRONT_L);
-		
-		s[s.length-1] = 1d / getDistanceSensorDataSmoothed(Sensor.FRONT_L);	// inversely proportional to support sharp curve to the left in case of 180° turns
-		
+
+		s[s.length-1] = 1d / getDistanceSensorDataSmoothed(Sensor.FRONT_L);	// inversely proportional to support sharp curve to the left in case of 180deg turns
+
 		double[] a = Matrix.add( Matrix.multiply(k, s), c );
-		
+
 		double leftValue = MAX_SPEED / (a[0] + a[1]) * a[0];
 		double rightValue = MAX_SPEED / (a[0] + a[1]) * a[1];
-		
+
 		setSpeed(leftValue, rightValue);
 	}
 }
